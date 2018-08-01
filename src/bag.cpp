2dde552a512839702205153bf28f18b8495b6e61
Eric Andrade <vloxflox@gmail.com>
2018-08-01 10:16:17 -0300

    não acredito que funcionou

--- src/bag.cpp
+++ src/bag.cpp
@@ -2,6 +2,7 @@
 #include "item.h"
 #include <vector>
 #include <iostream>
+#include <iterator>
 
 Bag::Bag(unsigned int sz){size = sz;}
 
@@ -19,5 +20,11 @@ void Bag::add_item(Item* item,int quantity){
 	add_me->quantity = quantity;
 
 	inside.push_back(*add_me);
+}
 
+Item* Bag::get_item(int item_id){
+	for(unsigned int i=0;i<inside.size();i++){
+		if(inside[i].stored->get_item_id() == item_id)return inside[i].stored;
+	}
+	return nullptr;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include "player.h"
 #include "map.h"
 #include "combat.h"
+#include "bag.h"
+#include "item.h"
 
 int main(){
-
+/*
 	Player eu(10,10,10,10);
 	Player vc(5,10,7,3);
 
@@ -12,7 +14,13 @@ int main(){
 	vc.set_name("John Wick");
 
 	Combat(&eu,&vc);
+*/
 
+Bag bog(15);
+Item espada(150);
+
+bog.add_item(&espada, 1);
+std::cout<<bog.get_item(150)->get_item_id()<<std::endl;
 
 
 //	Map mesa(5,5);
